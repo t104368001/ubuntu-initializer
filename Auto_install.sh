d5e0b7c922236a6be6b5fe652459e5e7529a33a8
taiyu-ken <31072035+taiyu-ken@users.noreply.github.com>
2017-09-21 11:40:23 -0400

    Add files via upload

--- Auto_install.sh
+++ Auto_install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 filename=~/Downloads/step.txt
 flag=0
-if [ -f "$filename" ];
-then
-	while IFS='' read -r line || [[ -n "$line" ]]; do
-	    if [[ $line == 0 ]]; then
+if [ -f "$filename" ];then #
+	while IFS='' read -r line || [[ -n "$line" ]]; do #
+		if [[ $line == 0 ]]; then #
 			echo "Ubuntu Initial"
 			echo "Just use Auto_install.sh until finish"
 			sudo apt-get -y upgrade
@@ -17,8 +16,8 @@ then
 			sudo dpkg -i sublime-text_build-3126_amd64.deb
 			echo '1' > ~/Downloads/step.txt
 			reboot
-		fi
-		if [ $line == 1 ]; then
+		fi #
+		if [ $line == 1 ]; then #
 			echo "Ros-Kinetic install"
 			sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 			sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
@@ -34,8 +33,8 @@ then
 			sudo apt-get -y install python-pip
 			pip install --upgrade pip
 			echo '2' >> ~/Downloads/step.txt
-		fi
-		if [ $line == 2 ]; then
+		fi #
+		if [ $line == 2 ]; then #
 			echo "Install cuda"
 			cd ~/Downloads
 			wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
@@ -47,19 +46,19 @@ then
 			echo "export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> ~/.bashrc
 			source ~/.bashrc
 			echo '3' >> ~/Downloads/step.txt
-		fi
-		if [ $line == 3 ]; then
+		fi #
+		if [ $line == 3 ]; then #
 			echo "Install cudnn"
 			cd ~/Download
 			wget https://www.dropbox.com/s/onxtezymyo9fq2q/cudnn-8.0-linux-x64-v5.1.tgz
-			tar -xvzf cudnn-8.0-linux-x64-v5.1.tgz 
+			tar -xvzf cudnn-8.0-linux-x64-v5.1.tgz
 			sudo cp -P cuda/include/cudnn.h /usr/local/cuda/include
 			sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda/lib64
 			sudo chmod a+r /usr/local/cuda/lib64/libcudnn*
 			echo '4' >> ~/Downloads/step.txt
 			reboot
-		fi
-		if [ $line == 4 ]; then
+		fi #
+		if [ $line == 4 ]; then #
 			echo "Install opencv"
 			cd ~
 			git clone https://github.com/daveselinger/opencv.git
@@ -76,8 +75,8 @@ then
 			make -j8
 			sudo make install
 			echo '5' >> ~/Downloads/step.txt
-		fi
-		if [ $line == 5 ]; then
+		fi #
+		if [ $line == 5 ]; then #
 			echo "Install bazel"
 			cd ~/Downloads
 			echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
@@ -92,30 +91,30 @@ then
 			echo "export PATH=\"$PATH:$HOME/bin\"" >> ~/.bashrc
 			source ~/.bashrc
 			sudo apt-get -y install python-numpy python-dev python-pip python-wheel
-			sudo apt-get -y install libcupti-dev 
+			sudo apt-get -y install libcupti-dev
 			echo '6' >> ~/Downloads/step.txt
-		fi
-		if [ $line == 6 ]; then
+		fi #
+		if [ $line == 6 ]; then #
 			echo "Install tensorflow"
 			cd ~
-			sudo apt-get -y install python-pip python-dev build-essential 
-			git clone https://github.com/tensorflow/tensorflow 
+			sudo apt-get -y install python-pip python-dev build-essential
+			git clone https://github.com/tensorflow/tensorflow
 			flag=1
 			echo '7' >> ~/Downloads/step.txt
-		fi
-		if [ $line == 7 ]; then
+		fi #
+		if [ $line == 7 ]; then #
 			echo "Already Finished !"
-		fi
-	done < "$filename"	
-	if [ $flag = 1 ]; then
+		fi #
+	done < "$filename" #
+	if [ $flag = 1 ]; then #
 		cd ~/tensorflow
 		./configure
 		bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
 		bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 		sudo pip install /tmp/tensorflow_pkg/tensorflow-1.2.0rc1-cp27-cp27mu-linux_x86_64.whl
 		echo "Finish !Congratulation ! And enjoy your system ^_^"
-	fi
-else
+	fi #
+else #
 	echo "Reset finish !"
 	echo '0' > ~/Downloads/step.txt
-fi
+fi #
